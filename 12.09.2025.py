4ee3ebec615f49ec662ece5e79e278aee0462114
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-09-12 16:17:34 +0500

    Create 12.09.2025.py

---
+++ 12.09.2025.py
@@ -0,0 +1,50 @@
+## 1
+# ism = 'Rasulbek'
+# print('salom', ism, '!')
+from traceback import print_tb
+
+## 2
+# yosh = 15
+# print('mening yoshim', yosh, '!')
+
+## 3
+# hozirgi_yil = 2025
+# yosh = 15
+# print(hozirgi_yil - yosh, "yilda tug'ulganman" )
+
+## 4
+# a = 10
+# b = 11
+# print("yig'ndi", a+b)
+
+## 5
+# uzunlik = 100
+# eni = 50
+# yuza = uzunlik * eni
+# print(yuza)
+
+## 6
+# radius = 10
+# formula = 2 * 3.14 * radius
+# print(formula)
+
+## 7
+# ism = 'Rasulbek'
+# yosh = 15
+# print('menig ismim', ism, 'mening yoshim', yosh )
+
+## 8
+# asos = 5
+# balandlik = 10
+# formula = (asos * balandlik) / 2
+# print(formula)
+
+## 9
+# son = 10
+# print(son**2)
+
+## 10
+# a = 10
+# b = 11
+# c = 12
+# print((a+b+c) / 3)
