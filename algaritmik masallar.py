4de9094780fc7f49272229e03843558bd5f1642f
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-09-19 14:21:30 +0300

    Add files via upload

---
+++ algaritmik masallar.py
@@ -0,0 +1,67 @@
+# # 1
+# son = float(input("iltimos, biror son kiriting: "))
+#
+# if son > 0:
+#     print(f"{son} - musbat son")
+#
+# elif son == 0:
+#     print(f"{son} - son nolga teng")
+#
+# else:
+#     print(f"{son} - manfiy son")
+
+## 2
+# son = int(input("1-sonni kiriting: "))
+# sonn = int(input("2-soni kirirting: "))
+#
+# if son - sonn < 0:
+#     print(f"kattasi{sonn}")
+#
+# elif son - sonn == 0:
+#     print(f"ikki son teng")
+#
+# else:
+#     print(f"kattasi {son}")
+
+## 3
+# son1 = int(input("1-sonni kiriting: "))
+# son2 = int(input("2-soni kiriting: "))
+# son3 = int(input("3-sonni kiriting: "))
+#
+# if son1 > son2 and son1 > son3:
+#     print(f"katta {son1}")
+#
+# elif son2 > son3 and son2 > son1:
+#     print(f"katta {son2}")
+#
+# elif son3 > son1 and son3 > son2:
+#     print(f"katta {son3}")
+#
+# else:
+#     print("xatolik ytuzberdi")
+
+## 4
+# son = int(input("soni kiriting: "))
+#
+# if son % 2 == 0:
+#     print("jusft son")
+#
+# elif son % 2 == 1:
+#     print("toq son ")
+#
+# else:
+#     print("xatolik berdi")
+
+## 5
+# son1 = int(input("1-soni kiriting: "))
+# son2 = int(input("2-soni kiriting: "))
+# son3 =  son1 + son2
+#
+# if son3 % 2 == 0:
+#     print("juft son ")
+#
+# elif son3 % 2 == 1:
+#     print("toq son")
+#
+# else:
+#     print("xatolik")
